ea36ffcfabdc0152d7a45bc7b532b6eaa638af9c
Brando <jose.brando.c.m@gmail.com>
2021-11-02 21:44:54 -0600

    Mostrar datos del controlador a la vista

--- src/main/java/com/example/demo/controllers/IndexController.java
+++ src/main/java/com/example/demo/controllers/IndexController.java
@@ -1,6 +1,7 @@
 package com.example.demo.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -11,8 +12,8 @@ import org.springframework.web.bind.annotation.RequestMethod;
 public class IndexController {
 	
 	@GetMapping({"/index", "/", "/home"})
-	public String index() {
-		
+	public String index(Model model) {
+		model.addAttribute("titulo", "Hola mundo!");
 		return "index";
 	}
 

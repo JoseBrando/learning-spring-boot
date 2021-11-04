42989c9fabc296e34c81ebf2f7756a194c80f5dd
Brando <jose.brando.c.m@gmail.com>
2021-11-03 23:21:51 -0600

    MVC

--- src/main/java/com/example/demo/controllers/IndexController.java
+++ src/main/java/com/example/demo/controllers/IndexController.java
@@ -6,15 +6,30 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import com.example.demo.models.Usuario;
+
 
 
 @Controller
+@RequestMapping("/app")
 public class IndexController {
 	
-	@GetMapping({"/index", "/", "/home"})
+	@GetMapping({"/index", "/", "", "/home"})
 	public String index(Model model) {
 		model.addAttribute("titulo", "Hola mundo!");
 		return "index";
+	}
+	
+	@RequestMapping("/perfil")
+	public String perfil(Model model) {
+		Usuario usuario = new Usuario();
+		
+		usuario.setNombre("Enrique");
+		usuario.setApellido("Juárez");
+		
+		model.addAttribute("usuario", usuario);
+		model.addAttribute("titulo", "Perfil del usuario: ".concat(usuario.getNombre()));
+		return "perfil";
 	}
 
 }
